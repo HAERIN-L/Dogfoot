4742a68954ecbd5de9b707c3c56a4fac2bc389be
HAERIN-L <dalllnim25@gmail.com>
2023-08-18 04:50:30 +0900

    front..

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+from flask import Flask, render_template, request, redirect, jsonify
 import os
 import re
 import subprocess
-from flask import Flask, render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/images'
@@ -22,18 +22,67 @@ def get_wifi_signal_strength():
 def index():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        filename = 'face.png'
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return redirect('/running')
+    else:
+        return redirect('/')
+
+
+@app.route('/running')
+def running():
+    return render_template('running.html')
+
 @app.route('/get_signal', methods=['GET'])
 def get_signal():
     signal_strength = get_wifi_signal_strength()
     return jsonify({'strength': signal_strength})
 
-@app.route('/', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    if file:
-        filename = 'face.png'
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect('/')
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# import os
+# import re
+# import subprocess
+# from flask import Flask, render_template, request, redirect, jsonify
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = 'static/images'
+
+# def get_wifi_signal_strength():
+#     try:
+#         output = subprocess.check_output(["netsh", "wlan", "show", "interfaces"],
+#                                          universal_newlines=True)
+#         match = re.search(r'\b신호\b\s+:\s+(\d+)%', output)
+#         if match:
+#             return int(match.group(1))
+#         else:
+#             return 0
+#     except subprocess.CalledProcessError:
+#         return 0
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/get_signal', methods=['GET'])
+# def get_signal():
+#     signal_strength = get_wifi_signal_strength()
+#     return jsonify({'strength': signal_strength})
+
+# @app.route('/', methods=['POST'])
+# def upload_file():
+#     file = request.files['file']
+#     if file:
+#         filename = 'face.png'
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#     return redirect('/')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,91 @@
-function updateSpeed() {
+// function updateSignalStrength() {
+//     fetch('/get_signal')
+//         .then(response => response.json())
+//         .then(data => {
+//             const signalStrengthElement = document.getElementById('signal-strength');
+//             signalStrengthElement.textContent = `${data.strength}`;
+
+//             updateRunnerPosition(data.strength);
+//         });
+// }
+
+// function updateRunnerPosition() {
+
+//     const runner = document.querySelector('.runner');
+
+//     let newPosition = (strength / 100)
+//     fetch('/get_signal')
+//         .then(response => response.json())
+//         .then(data => {
+//             const runner = document.querySelector('.runner');
+
+//             let newPosition = (data.strength / 100) * 90;  // 90%는 목적지 바로 앞까지의  거리
+//             runner.style.left = `calc(${newPosition}% - 100px)`;
+            
+//             if (data.strength >= 50) {
+//                 runner.classList.add('running');
+//             } else {
+//                 runner.classList.remove('running');
+//             }
+//         });
+// }
+
+// setInterval(updateSignalStrength, 1000);
+// setInterval(updateRunnerPosition, 1000);
+
+// //============================================
+// function updateSignalStrength() {
+//     fetch('/get_signal')
+//         .then(response => response.json())
+//         .then(data => {
+//             const signalStrengthElement = document.getElementById('signal-strength');
+//             signalStrengthElement.textContent = `${data.strength}`;
+//             updateRunnerPosition(data.strength); // 위치 업데이트 함수에 신호 강도를 전달
+//         });
+// }
+
+// function updateRunnerPosition(strength) {
+//     const runner = document.querySelector('.runner');
+//     let newPosition = (strength / 100) * 90;  // 와이파이 신호 강도에 따라 캐릭터 위치 계산
+    
+//     runner.style.left = `calc(${newPosition}% - 100px)`; // 캐릭터의 새 위치를 설정
+
+//     if (strength >= 50) {
+//         runner.classList.add('running');
+//     } else {
+//         runner.classList.remove('running');
+//     }
+// }
+
+// setInterval(updateSignalStrength, 1000); // 1초마다 와이파이 신호 강도를 업데이트
+
+
+function updateSignalStrength() {
     fetch('/get_signal')
         .then(response => response.json())
         .then(data => {
-            let speed = Math.max(1, 10 - data.strength / 10);
-            document.getElementById('character').style.animationDuration = `${speed}s`;
-        })
-        .catch(error => console.error('Error fetching signal strength:', error));
+            const signalStrengthElement = document.getElementById('signal-strength');
+            signalStrengthElement.textContent = `${data.strength}`;
+
+            updateRunnerPosition(data.strength);
+        });
+}
+function updateRunnerPosition(strength) {
+    const runner = document.querySelector('.runner');
+
+    let newPosition = (strength / 100) * 90; 
+    runner.style.left = `calc(${newPosition}% - 100px)`; // 천천히 이동
+
+    if (strength >= 50) {
+        runner.classList.add('running');
+    } else {
+        runner.classList.remove('running');
+    }
 }
 
+// 페이지가 로드될 때 한 번만 실행
+updateSignalStrength();
+// 그 이후에는 주기적으로 실행
+setInterval(updateSignalStrength, 1000);
 
-setInterval(updateSpeed, 2000);
+
